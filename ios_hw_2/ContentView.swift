2b2436fbc2c6c1f2b0f8e521f6fa4759d1d4ee0d
gogo <eric27202660@gmail.com>
2021-11-07 02:39:59 +0800

    Initial Commit

---
+++ ios_hw_2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_hw_2
+//
+//  Created by 官鼎鈞 on 2021/11/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
